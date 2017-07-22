f5947ef31d218ef2478d14aacea296b08ea6e0f4
Anna Kuznetsova <aakuznetsova@gmail.com>
2017-07-21 22:18:16 -0700

    implemented new database schema

--- media.sql
+++ media.sql
@@ -1,20 +1,33 @@
 CREATE TABLE animals (
-    id serial PRIMARY KEY,
-    name character varying
+  id           SERIAL PRIMARY KEY,
+  name         VARCHAR NOT NULL,
+  isEndangered BOOLEAN NOT NULL
 );
 
-CREATE TABLE endangered_animals (
-    id serial PRIMARY KEY,
-    name character varying,
-    health character varying,
-    age character varying
+CREATE TABLE locations (
+  id   SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL
+);
+
+CREATE TABLE stations (
+  id          SERIAL PRIMARY KEY,
+  name        VARCHAR NOT NULL,
+  location_id INTEGER NOT NULL REFERENCES locations (id)
+);
+
+CREATE TABLE rangers (
+  id         SERIAL PRIMARY KEY,
+  name       VARCHAR NOT NULL,
+  station_id INTEGER NOT NULL REFERENCES stations (id)
 );
 
 CREATE TABLE sightings (
-    id serial PRIMARY KEY,
-    animal_id integer REFERENCES animals(id),
-    location character varying,
-    ranger_name character varying
-
+  id          SERIAL PRIMARY KEY,
+  animal_id   INTEGER   NOT NULL REFERENCES animals (id),
+  health      VARCHAR,
+  age         VARCHAR,
+  ranger_id   INTEGER   NOT NULL REFERENCES rangers (id),
+  location_id INTEGER   NOT NULL REFERENCES locations (id),
+  time        TIMESTAMP NOT NULL,
+  image       VARCHAR
 );
-
